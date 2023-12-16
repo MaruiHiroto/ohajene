a44cb84f081cf9a78e24dad561f10be1942de8b5
MaruiHiroto <torohorototo@gmail.com>
2023-12-17 00:23:42 +0900

    [Add] index

---
+++ chihaya.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>oha-jene joke</title>
+</head>
+<body>
+    <h1>おはなしじぇねれーたー偽</h1>
+    <hr>
+    <img src="face_765/face_haruka/haruka_normal.png" width="4%">
+    <img src="face_765/face_haruka/haruka_ayasimi.png" width="4%">
+    <img src="face_765/face_haruka/haruka_huzake.png" width="4%">
+    <img src="face_765/face_haruka/haruka_tere.png" width="4%">
+    <img src="face_765/face_haruka/haruka_oowarai.png" width="4%">
+    &nbsp;
+    <img src="face_765/face_makoto/makoto_normal.png" width="4%">
+    <img src="face_765/face_makoto/makoto_ibukasimi.png" width="4%">
+    <img src="face_765/face_makoto/makoto_tere.png" width="4%">
+    <img src="face_765/face_makoto/makoto_tere2.png" width="4%">
+    <img src="face_765/face_makoto/makoto_yarii.png" width="4%">
+    &nbsp;
+    <img src="face_765/face_iori/iori_normal.png" width="4%">
+    <img src="face_765/face_iori/iori_kurai.png" width="4%">
+    <img src="face_765/face_iori/iori_okori.png" width="4%">
+    <img src="face_765/face_iori/iori_oowarai.png" width="4%">
+    <img src="face_765/face_iori/iori_takabisya.png" width="4%">
+    &nbsp;
+    <img src="face_765/face_ritsuko/ritsuko_normal.png" width="4%">
+    <img src="face_765/face_ritsuko/ritsuko_ankokubisyou.png" width="4%">
+    <img src="face_765/face_ritsuko/ritsuko_gimon.png" width="4%">
+    <img src="face_765/face_ritsuko/ritsuko_odoroki.png" width="4%">
+    <img src="face_765/face_ritsuko/ritsuko_yaruki.png" width="4%">
+    <p></p>
+    <img src="face_765/face_chihaya/chihaya_normal.png" width="4%">
+    <img src="face_765/face_chihaya/chihaya_hukigen.png" width="4%">
+    <img src="face_765/face_chihaya/chihaya_kultu.png" width="4%">
+    <img src="face_765/face_chihaya/chihaya_rage.png" width="4%">
+    <img src="face_765/face_chihaya/chihaya_warai.png" width="4%">
+    &nbsp;
+    <img src="face_765/face_yukiho/yukiho_normal.png" width="4%">
+    <img src="face_765/face_yukiho/yukiho_egao.png" width="4%">
+    <img src="face_765/face_yukiho/yukiho_kiduki.png" width="4%">
+    <img src="face_765/face_yukiho/yukiho_otikomi.png" width="4%">
+    <img src="face_765/face_yukiho/yukiho_uresii.png" width="4%">
+    &nbsp;
+    <img src="face_765/face_azusa/azusa_normal.png" width="4%">
+    <img src="face_765/face_azusa/azusa_gimon.png" width="4%">
+    <img src="face_765/face_azusa/azusa_otona.png" width="4%">
+    <img src="face_765/face_azusa/azusa_terewarai.png" width="4%">
+    <img src="face_765/face_azusa/azusa_warai.png" width="4%">
+    &nbsp;
+    <img src="face_765/face_takane/takane_normal.png" width="4%">
+    <img src="face_765/face_takane/takane_harapeko.png" width="4%">
+    <img src="face_765/face_takane/takane_hungai.png" width="4%">
+    <img src="face_765/face_takane/takane_odoroki.png" width="4%">
+    <img src="face_765/face_takane/takane_uresii.png" width="4%">
+
+    <p></p>
+    <img src="face_765/face_miki/miki_normal.png" width="4%">
+    <img src="face_765/face_miki/miki_iyanano.png" width="4%">
+    <img src="face_765/face_miki/miki_nano.png" width="4%">
+    <img src="face_765/face_miki/miki_nemuino.png" width="4%">
+    <img src="face_765/face_miki/miki_tere.png" width="4%">
+    &nbsp;
+    <img src="face_765/face_yayoi/yayoi_normal.png" width="4%">
+    <img src="face_765/face_yayoi/yayoi_kawaii.png" width="4%">
+    <img src="face_765/face_yayoi/yayoi_odoroki.png" width="4%">
+    <img src="face_765/face_yayoi/yayoi_oowarai.png" width="4%">
+    <img src="face_765/face_yayoi/yayoi_warai.png" width="4%">
+    &nbsp;
+    <img src="face_765/face_ami/ami_normal.png" width="4%">
+    <img src="face_765/face_ami/ami_aogu.png" width="4%">
+    <img src="face_765/face_ami/ami_ibukasimi.png" width="4%">
+    <img src="face_765/face_ami/ami_oowarai.png" width="4%">
+    <img src="face_765/face_ami/ami_tere.png" width="4%">
+    &nbsp;
+    <img src="face_765/face_hibiki/hibiki_normal.png" width="4%">
+    <img src="face_765/face_hibiki/hibiki_kurai.png" width="4%">
+    <img src="face_765/face_hibiki/hibiki_oowarai.png" width="4%">
+    <img src="face_765/face_hibiki/hibiki_tere.png" width="4%">
+    <img src="face_765/face_hibiki/hibiki_warai.png" width="4%">
+
+    <p></p>
+    <img src="face_765/face_mami/mami_normal.png" width="4%">
+    <img src="face_765/face_mami/mami_itadura.png" width="4%">
+    <img src="face_765/face_mami/mami_komari.png" width="4%">
+    <img src="face_765/face_mami/mami_odoroki.png" width="4%">
+    <img src="face_765/face_mami/mami_warai.png" width="4%">
+    <hr>
+    <div class="optidol">
+        <form action='result.php' method='POST'>
+            <label for='idol_opt'>アイドル</label>
+            <select name="idol_opt">
+            <option value="haruka">天海春香</option>    
+            <option value="chihaya">如月千早</option>
+            <option value="miki">星井美希</option>
+            <option value="iori">水瀬伊織</option>
+            <option value="takane">四条貴音</option>
+            <option value="hibiki">我那覇響</option>
+            <option value="ritsuko">秋月律子</option>
+            <option value="azusa">三浦あずさ</option>
+            <option value="ami">双海亜美</option>
+            <option value="mami">双海真美</option>
+            <option value="yayoi">高槻やよい</option>
+            <option value="makoto">菊地真</option>
+            <option value="yukiho">萩原雪歩</option>
+            <option value="P">P</option>
+            </select>
+            <label for='idol_hyoujou' method='POST'>番号</label>
+            <select name="idol_hyoujou">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+            </select>
+            <p></p>
+            <label for='serif' method='POST'>セリフ</label>
+            <input type='text' size="78" name='serif'>
+            <button type='submit'>追加</button>
+        </form>
+        <form action='delete.php' method='POST'>
+            <label for='article'>削除する行</label>
+            <input type='txt' name='int'>
+            <button type='submit'>削除</button> 
+        </form>
+        <form action='irekae.php' method='POST'>
+            <label for='article'>入れ替える:前の行</label>
+            <input type='int' name='mae'>
+            <label for='article'>後の行</label>
+            <input type='int' name='ato'>
+            <button type='submit'>入れ替える</button>
+        </form>
+        <form action="allcreare.php" method="POST">
+            <button type='submit'>全削除</button>
+        </form>
+        
+        <hr>
+        <p>
+            <?php if (isset($plt_path)) {?>
+                <p><img src="<?php echo $plt_path ?>" alt="エラーです" width="25%"></p>
+            <?php } ?>
+        </p>
+        
+    </div>
+</body>
+</html>
