75bee440db4abf27437d4e020122b9b185f9fed4
MaruiHiroto <torohorototo@gmail.com>
2023-12-24 12:54:57 +0900

    sounyuu add

--- irekae.php
+++ irekae.php
@@ -1,22 +1,18 @@
 <?php
-    $mae_bangou = $_POST['mae'];
-    $ato_bangou = $_POST['ato'];
+    $mae_bangou = $_POST['taisyou'];
     $lines = file(__DIR__ . '/articles.txt', FILE_IGNORE_NEW_LINES);
     $idols = file(__DIR__ . '/idol.txt', FILE_IGNORE_NEW_LINES);
     $hyoujou_list = file(__DIR__ . '/hyoujou.txt', FILE_IGNORE_NEW_LINES);
     $idols_count = count($idols);
-    if ($mae_bangou < $ato_bangou && $ato_bangou <= $idols_count && $mae_bangou != $ato_bangou && isset($mae_bangou) && isset($ato_bangou)){
-        $souko = $lines[$mae_bangou-1];
-        $lines[$mae_bangou-1] = $lines[$ato_bangou-1];
-        $lines[$ato_bangou-1] = $souko;
+    $ato_bangou = $idols_count;
+    if ($mae_bangou != $ato_bangou && isset($mae_bangou) && isset($ato_bangou) && $mae_bangou <= $idols_count && $ato_bangou <= $idols_count){
 
-        $souko_i = $idols[$mae_bangou-1];
-        $idols[$mae_bangou-1] = $idols[$ato_bangou-1];
-        $idols[$ato_bangou-1] = $souko_i;
-
-        $souko_j = $hyoujou_list[$mae_bangou-1];
-        $hyoujou_list[$mae_bangou-1] = $hyoujou_list[$ato_bangou-1];
-        $hyoujou_list[$ato_bangou-1] = $souko_j;
+        array_splice($lines, $mae_bangou-1, 0, $lines[$ato_bangou-1]);
+        array_splice($idols, $mae_bangou-1, 0, $idols[$ato_bangou-1]);
+        array_splice($hyoujou_list, $mae_bangou-1, 0, $hyoujou_list[$ato_bangou-1]);
+        array_splice($lines, $idols_count);
+        array_splice($idols, $idols_count);
+        array_splice($hyoujou_list, $idols_count);
 
         file_put_contents(__DIR__ . '/articles.txt', "", LOCK_EX);
         file_put_contents(__DIR__ . '/idol.txt', "", LOCK_EX);
